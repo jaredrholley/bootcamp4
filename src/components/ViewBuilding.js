55dcb60811cbebb310f8737bbe3cab4b7e306ed1
Jared Holley <jaredrholley@ufl.edu>
2019-10-24 14:40:26 -0400

    Added view more info textbox

--- src/components/ViewBuilding.js
+++ src/components/ViewBuilding.js
@@ -3,22 +3,37 @@ import React from 'react';
 class ViewBuilding extends React.Component {
 	render() {
 
-		var buildingInfo;
+		var headerInfo;
+		var buildingInfo = "";
 
 		if (this.props.selectedBuilding !== 0) {
-			var buildingToDisplay = this.props.data.find((building) => { return (building.id === this.props.selectedBuilding) } );
-			buildingInfo = <i>{buildingToDisplay.name}</i>
+			buildingInfo = this.props.data.find((building) => { return (building.id === this.props.selectedBuilding) } );
+			headerInfo = <b>{buildingInfo.name}</b>
 		}
 		else 
 		{
-			buildingInfo = <i>Click on a name to view more information</i>
+			headerInfo = <i>Click on a name to view more information</i>
+
+			return (
+				<div>
+					<p>
+						{' '}
+						<h3>{headerInfo}</h3>
+					</p>
+				</div>
+			);
+
 		}
 
 		return (
 			<div>
 				<p>
 					{' '}
-					{buildingInfo}
+					<h3>{headerInfo}</h3>
+					<td>Building Code :  {buildingInfo.code}</td><br/>
+					<td>Address : {buildingInfo.address}</td><br/>
+					<td>Latitude : {buildingInfo.coordinates.latitude}</td><br/>
+					<td>Longitude : {buildingInfo.coordinates.longitude}</td><br/>
 				</p>
 			</div>
 		);
